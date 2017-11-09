065d3f532058d8cc599d275668a7ed8eb4be5d4e
Rémy HUBSCHER <rhubscher@mozilla.com>
2017-11-09 17:23:27 +0100

    Implement From for Message

--- redisish/src/server.rs
+++ redisish/src/server.rs
@@ -14,27 +14,6 @@ pub struct Redisish {
 }
 
 impl Redisish {
-    fn handle_message(&mut self, content: String) -> Message {
-        let content = String::from(content.trim());
-        let mut parts = content.splitn(3, ' ');
-        
-        match (parts.next(), parts.next(), parts.next()) {
-            (Some("RETRIEVE"), None, None) => {
-                Message::Retrieve(String::from("default"))
-            },
-            (Some("RETRIEVE"), Some(channel), None) => {
-                Message::Retrieve(String::from(channel))
-            },
-            (Some("PUSH"), Some(value), None) => {
-                Message::Push(String::from("default"), String::from(value))
-            },
-            (Some("PUSH"), Some(channel), Some(value)) => {
-                Message::Push(String::from(channel), String::from(value))
-            },
-            (_, _, _) => Message::Invalid(content.clone())
-        }
-    }
-    
     pub fn handle_client(&mut self, stream: TcpStream) {
         let mut stream = BufStream::new(stream);
 
@@ -47,39 +26,34 @@ impl Redisish {
                     if content.len() == 0 {
                         break;
                     }
-                    let message = self.handle_message(content);
+                    let message = Message::from(content);
 
                     match message {
                         Message::Retrieve(channel) => {
-                            let mut queue = self.channels.entry(String::from(channel))
+                            let mut queue = self.channels.entry(channel.clone())
                                 .or_insert(VecDeque::new());
 
                             match queue.pop_front() {
                                 Some(value) => {
-                                    stream.write(&value.as_bytes()).unwrap();
-                                    stream.write(b"\n").unwrap();
+                                    write!(stream, "+ {}\n", value).unwrap();
                                 },
                                 None => {
-                                    stream.write(b"- Empty Queue\n").unwrap();
+                                    write!(stream, "- Queue `{}` is empty.\n", channel).unwrap();
                                 }
                             }
                         },
                         Message::Push(channel, value) => {
-                            let mut queue = self.channels.entry(String::from(channel))
+                            let mut queue = self.channels.entry(channel)
                                 .or_insert(VecDeque::new());
                             queue.push_front(value);
                             stream.write(b"+ OK\n").unwrap();
                         },
                         Message::Invalid(content) => {
-                            stream.write(b"- Unknown command: ").unwrap();
-                            stream.write(content.as_bytes()).unwrap();
-                            stream.write(b"\n").unwrap();
+                            write!(stream, "- Unknown command: {}\n", content).unwrap();
                         }
                     }
                     match stream.flush() {
-                        Err(_) => {
-                            break;
-                        },
+                        Err(_) => break,
                         _ => {}
                     }
                 },
--- redisish/src/types.rs
+++ redisish/src/types.rs
@@ -1,3 +1,5 @@
+pub const DEFAULT_CHANNEL: &'static str = "default";
+
 pub type Channel = String;
 pub type Value = String;
 
@@ -5,4 +7,28 @@ pub enum Message {
     Retrieve(Channel),
     Push(Channel, Value),
     Invalid(String),
+}
+
+
+impl From<String> for Message {
+    fn from(raw: String) -> Self {
+        let content = raw.trim();
+        let mut parts = content.splitn(3, ' ');
+
+        match (parts.next(), parts.next(), parts.next()) {
+            (Some("RETRIEVE"), None, None) => {
+                Message::Retrieve(String::from(DEFAULT_CHANNEL))
+            },
+            (Some("RETRIEVE"), Some(channel), None) => {
+                Message::Retrieve(String::from(channel))
+            },
+            (Some("PUSH"), Some(value), None) => {
+                Message::Push(String::from(DEFAULT_CHANNEL), String::from(value))
+            },
+            (Some("PUSH"), Some(channel), Some(value)) => {
+                Message::Push(String::from(channel), String::from(value))
+            },
+            (_, _, _) => Message::Invalid(String::from(content))
+        }
+    }
 }
