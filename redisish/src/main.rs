f45185bccde3d12cb50a96c096f389b6f694cc23
Rémy HUBSCHER <rhubscher@mozilla.com>
2017-11-10 10:55:16 +0100

    Refactoring redishish for threading.

--- redisish/src/main.rs
+++ redisish/src/main.rs
@@ -1,24 +1,24 @@
 extern crate bufstream;
 mod types;
 mod server;
-
-use server::Redisish;
+use server::{Redisish, handle_client};
 use std::net::TcpListener;
 use std::thread;
 use std::sync::Arc;
+use std::sync::Mutex;
 
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:8888").expect("Couldn't bind 127.0.0.1:8888");
     listener.set_ttl(10).expect("could not set TTL");
     println!("Server started at 127.0.0.1:8888");
-    let mut redisish_server = Arc::new(Redisish::new());
+    let mut redisish_server = Arc::new(Mutex::new(Redisish::default()));
     // accept connections and process them serially
     for stream in listener.incoming() {
         println!("New connection");
+        let mut server = Arc::clone(&redisish_server);
         thread::spawn(move || {
-            let server = Arc::clone(&redisish_server);
-            server.handle_client(stream.expect("Stream is not usable"));
+            handle_client(&mut server, stream.unwrap());
         });
     }
 }
--- redisish/src/server.rs
+++ redisish/src/server.rs
@@ -1,73 +1,70 @@
-use std::net::TcpStream;
 use std::io::{BufRead, Write};
+use std::net::TcpStream;
 use std::collections::HashMap;
 use std::collections::VecDeque;
 use bufstream::BufStream;
 use types::Channel;
 use types::Value;
 use types::Message;
-
+use std::sync::Arc;
 use std::sync::Mutex;
 
-
+#[derive(Default)]
 pub struct Redisish {
-    channels: Mutex<HashMap<Channel, VecDeque<Value>>>,
+    channels: HashMap<Channel, VecDeque<Value>>,
 }
 
 impl Redisish {
 
-    pub fn new() -> Redisish {
-        Redisish { channels: Mutex::new(HashMap::new()) }
+    pub fn command(&mut self, message: Message) -> String {
+        match message {
+            Message::Retrieve(channel) => {
+                let mut queue = self.channels.entry(channel.clone())
+                    .or_insert(VecDeque::new());
+                match queue.pop_front() {
+                    Some(value) => {
+                        format!("+ {}\n", value)
+                    },
+                    None => {
+                        format!("- Queue `{}` is empty.\n", channel)
+                    }
+                }
+            },
+            Message::Push(channel, value) => {
+                let mut queue = self.channels.entry(channel.clone())
+                    .or_insert(VecDeque::new());
+                queue.push_front(value);
+                format!("+ OK\n")
+            },
+            Message::Invalid(content) => {
+                format!("- Unknown command: {}\n", content)
+            }
+        }
     }
+}
 
-    pub fn handle_client(&mut self, stream: TcpStream) {
-        let mut stream = BufStream::new(stream);
+pub fn handle_client(server: &mut Arc<Mutex<Redisish>>, stream: TcpStream) {
+    let mut stream = BufStream::new(stream);
 
-        loop {
-            let mut content = String::new();
-            let line = stream.read_line(&mut content);
-
-            match line {
-                Ok(_) => {
-                    if content.len() == 0 {
-                        break;
-                    }
-                    let message = Message::from(content);
-
-                    match message {
-                        Message::Retrieve(channel) => {
-                            let mut channels_hashmap = self.channels.lock().unwrap();
-                            let mut queue = channels_hashmap.entry(channel.clone())
-                                .or_insert(VecDeque::new());
-
-                            match queue.pop_front() {
-                                Some(value) => {
-                                    write!(stream, "+ {}\n", value).unwrap();
-                                },
-                                None => {
-                                    write!(stream, "- Queue `{}` is empty.\n", channel).unwrap();
-                                }
-                            }
-                        },
-                        Message::Push(channel, value) => {
-                            let mut channels_hashmap = self.channels.lock().unwrap();
-                            let mut queue = channels_hashmap.entry(channel)
-                                .or_insert(VecDeque::new());
-                            queue.push_front(value);
-                            stream.write(b"+ OK\n").unwrap();
-                        },
-                        Message::Invalid(content) => {
-                            write!(stream, "- Unknown command: {}\n", content).unwrap();
-                        }
-                    }
-                    match stream.flush() {
-                        Err(_) => break,
-                        _ => {}
-                    }
-                },
-                Err(_) => {
+    loop {
+        let mut content = String::new();
+        let line = stream.read_line(&mut content);
+        match line {
+            Ok(_) => {
+                if content.len() == 0 {
                     break;
                 }
+                let message = Message::from(content);
+                let mut s = server.lock().unwrap();
+                let response = s.command(message);
+                stream.write(response.as_bytes()).unwrap();
+                match stream.flush() {
+                    Err(_) => break,
+                    _ => {}
+                }
+            },
+            Err(_) => {
+                break;
             }
         }
     }
